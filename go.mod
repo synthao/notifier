0cebd6bc055f8320d392adbc3493328942928c05
Maksim Pikalov <sew810i9@gmail.com>
2024-03-24 00:16:58 +0700

    add Kafka consumer to read messages from the orders service

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/synthao/notifier
+
+go 1.21.3
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/segmentio/kafka-go v0.4.47 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/segmentio/kafka-go"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+type notificationsTopicMsg struct {
+	OrderID        int `json:"order_id"`
+	PreviousStatus int `json:"previous_status"`
+	Status         int `json:"status"`
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	logger, err := newLogger(os.Getenv("LOG_LEVEL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:  []string{os.Getenv("KAFKA_BROKER_HOST")},
+		Topic:    os.Getenv("KAFKA_TOPIC"),
+		MaxBytes: 10e6, // 10MB4
+		GroupID:  os.Getenv("KAFKA_GROUP_ID"),
+	})
+
+	// Set up a channel to receive OS signals for graceful shutdown
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigChan
+		fmt.Println("Received shutdown signal")
+		cancel()
+		if err := reader.Close(); err != nil {
+			logger.Error("closing kafka reader error", zap.Error(err))
+		}
+	}()
+
+	for {
+		m, err := reader.ReadMessage(ctx)
+		if err != nil {
+			if errors.Is(err, context.Canceled) {
+				break
+			}
+			logger.Error("reading message error", zap.Error(err))
+			continue
+		}
+
+		var msg notificationsTopicMsg
+
+		if err := json.Unmarshal(m.Value, &msg); err != nil {
+			logger.Error("failed to unmarshal message", zap.Error(err), zap.ByteString("data", m.Value))
+			continue
+		}
+
+		if err := reader.CommitMessages(ctx, m); err != nil {
+			logger.Error("commit message error", zap.Error(err))
+			continue
+		}
+
+		logger.Info("message processed", zap.ByteString("data", m.Value))
+	}
+}
+
+func newLogger(level string) (*zap.Logger, error) {
+	atomicLogLevel, err := zap.ParseAtomicLevel(level)
+	if err != nil {
+		return nil, err
+	}
+
+	atom := zap.NewAtomicLevelAt(atomicLogLevel.Level())
+	encoderCfg := zap.NewProductionEncoderConfig()
+	encoderCfg.TimeKey = "timestamp"
+	encoderCfg.EncodeTime = zapcore.ISO8601TimeEncoder
+
+	return zap.New(
+		zapcore.NewCore(
+			zapcore.NewJSONEncoder(encoderCfg),
+			zapcore.Lock(os.Stdout),
+			atom,
+		),
+		zap.WithCaller(true),
+		zap.AddStacktrace(zap.ErrorLevel),
+	), nil
+}
